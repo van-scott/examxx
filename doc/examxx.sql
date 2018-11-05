60bc89d70931a22cbf5a91520fd1e8f0fe63515d
xinln <xinln@esensoft.com>
2018-11-05 16:12:40 +0800

    CHG:修改工程名称

--- doc/examxx.sql
+++ doc/examxx.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50617
 Source Host           : localhost:3306
-Source Database       : examxx
+Source Database       : netschool-exam
 
 Target Server Type    : MYSQL
 Target Server Version : 50617
